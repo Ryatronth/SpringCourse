14c31d4bf2889264789456695a9cad22f6c094ff
Ryatronth <roman-novikov-2904@mail.ru>
2024-03-02 13:38:40 +0500

    Поправил ошибки

--- src/main/java/org/example/springcourse/models/Boss.java
+++ src/main/java/org/example/springcourse/models/Boss.java
@@ -1,9 +1,24 @@
 package org.example.springcourse.models;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.stereotype.Component;
+
+@Component("boss")
 public class Boss implements Worker{
 
     @Override
     public void work() {
         System.out.println("Я заставляю программиста писать код.");
+    }
+
+    @PostConstruct
+    public void construct() {
+        System.out.println("Босс создан");
+    }
+
+    @PreDestroy
+    public void destroy() {
+        System.out.println("Босс уничтожен");
     }
 }
--- src/main/java/org/example/springcourse/services/WorkService.java
+++ src/main/java/org/example/springcourse/services/WorkService.java
@@ -4,28 +4,28 @@ import org.example.springcourse.models.Boss;
 import org.example.springcourse.models.Programmer;
 import org.example.springcourse.models.Worker;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
  class WorkService {
-
-    private Programmer programmer;
-    private Programmer programmer1;
+    private Worker programmer;
+    private Worker programmer1;
     // DI через поле
     @Autowired
-    private Boss boss;
+    @Qualifier("boss")
+    private Worker boss;
 
 
     // DI через конструктор
-    @Autowired
-    public WorkService(Programmer programmer) {
+    public WorkService(@Qualifier("programmer") Worker programmer) {
         this.programmer = programmer;
     }
 
-    // DI ерез сеттер
+    // DI через сеттер
     @Autowired
-    public void setProgrammer1(Programmer programmer1) {
+    public void setProgrammer1(@Qualifier("programmer") Worker programmer1) {
         this.programmer1 = programmer1;
     }
 
